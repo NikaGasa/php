2b46f43bef785025b3e8010dde65340117459fb6
NikaGasa <nikoloz.gasashvili@gmail.com>
2022-09-30 16:21:47 +0400

    first comm

---
+++ hm1.php
@@ -0,0 +1,17 @@
+<?php
+
+    $number_1 = (int)readline("input number from 1 till 100: ");
+   $number_2 = (int)readline("input number from 1 till 100: ");
+   
+   echo $number_1 . "\n";
+   echo($number_2. "\n");
+   
+    if ($number_1 >100 or $number_2 > 100){
+      echo "Numbers must be between 1 and 100";
+    }elseif($number_1 == 0 or $number_2 == 0){
+      echo "nervebi momishale";
+    }else{
+        echo ($number_1 * $number_2);
+  }
+    
+?>
